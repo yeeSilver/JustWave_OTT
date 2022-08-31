25d16c993adb921d5416bf2175534a8656c19bef
yeeun_An <an446@naver.com>
2022-08-31 17:42:07 +0900

    :sparkles: Slider > Row > Box 구현

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,7 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchMovies, IFetchMoviesResult } from "../api";
 import styled from "styled-components";
+import { motion, AnimatePresence } from "framer-motion";
 import { makeImgPath } from "../utilities";
+import { useState } from "react";
 
 const Wrapper = styled.div`
   background-color: black;
@@ -35,12 +37,59 @@ const Overview = styled.p`
   font-size: 30px;
 `;
 
+const Slider = styled.div`
+  position: relative;
+  top: -100px;
+`;
+
+const Row = styled(motion.div)<{ bgImg: string }>`
+  display: grid;
+  gap: 5px;
+  grid-template-columns: repeat(6, 1fr);
+  position: absolute;
+  width: 100%;
+  background-image: url(${(props) => props.bgImg});
+  background-size: cover;
+  background-position: center center;
+`;
+
+const Box = styled(motion.div)`
+  background-color: white;
+  height: 200px;
+`;
+
+const rowVariants = {
+  hidden: {
+    x: window.outerWidth + 5,
+  },
+  visible: {
+    x: 0,
+  },
+  exit: {
+    x: -window.outerWidth - 5,
+  },
+};
+
+const offset = 6;
+
 export default function Home() {
   const { data, isLoading } = useQuery<IFetchMoviesResult>(
     ["movies", "nowPlaying"],
     fetchMovies
   );
-  console.log(data?.results[0].backdrop_path);
+  const [index, setIndex] = useState(0);
+  const [leaving, setLeaving] = useState(false);
+  const toggleLeaving = () => setLeaving((prev) => !prev);
+  const increaseIndex = () => {
+    if (data) {
+      if (leaving) return;
+      toggleLeaving();
+      const totalMovies = data?.results.length - 1;
+      const maxIndex = Math.ceil(totalMovies / offset) - 1;
+      setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
+    }
+  };
+
   return (
     <Wrapper>
       {isLoading ? (
@@ -51,6 +100,29 @@ export default function Home() {
             <Title>{data?.results[0].title}</Title>
             <Overview>{data?.results[0].overview}</Overview>
           </Banner>
+          <Slider>
+            <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
+              <Row
+                onClick={increaseIndex}
+                key={index}
+                variants={rowVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                transition={{ type: "tween", duration: 1 }}
+              >
+                {data?.results
+                  .slice(1)
+                  .slice(offset * index, offset * index + offset)
+                  .map((movie) => (
+                    <Box
+                      key={movie.id}
+                      bgImg={makeImgPath(movie.backdrop_path, w500)}
+                    ></Box>
+                  ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
         </>
       )}
     </Wrapper>
